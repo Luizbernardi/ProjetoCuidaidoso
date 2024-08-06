0d2e1fb84af6729055c76e2efd6091669b602e8b
Luiz Bernardi <luizbernardi37@gmail.com>
2024-08-06 10:05:13 -0300

    ajustando intellisense tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        redlight: '#E55959',
+        greenlight: '#7FCDCD',
+        graylight: '#E7E7E7'
+      }
+    }
+  },
+  plugins: [],
+}
+
