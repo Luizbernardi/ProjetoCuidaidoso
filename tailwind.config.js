739040b6d28d1ee5970f90edb8127a2db7517f9a
Luiz Bernardi <luizbernardi37@gmail.com>
2024-08-06 18:34:13 -0300

    mudança de nome dos arquivos

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,9 @@ module.exports = {
         redlight: '#E55959',
         greenlight: '#7FCDCD',
         graylight: '#E7E7E7',
-        graytext: '#D9D9D9'
+        graytext: '#D9D9D9',
+        redbackground: '#EED0D0',
+        orangetext: '#C25500'
       }
     }
   },
