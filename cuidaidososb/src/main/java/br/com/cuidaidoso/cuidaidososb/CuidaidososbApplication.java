a77cbbf327e5d56b5acf97019ccb6c45ff283351
Luiz Bernardi <luizbernardi37@gmail.com>
2024-08-15 16:29:40 -0300

    feat(arrumando):Organizando Esqueleto do projeto

---
+++ cuidaidososb/src/main/java/br/com/cuidaidoso/cuidaidososb/CuidaidososbApplication.java
@@ -0,0 +1,13 @@
+package br.com.cuidaidoso.cuidaidososb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuidaidososbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuidaidososbApplication.class, args);
+	}
+
+}
---
+++ cuidaidososb/src/test/java/br/com/cuidaidoso/cuidaidososb/CuidaidososbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cuidaidoso.cuidaidososb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuidaidososbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
